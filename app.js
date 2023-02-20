de2cd02e5958242569cadcb7ba3e4015f52cf617
ChaseDev <chasewebdev101@gmail.com>
2023-02-20 14:15:15 -0800

    Added player and computer selections

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const gameArray = ["rock", "paper", "scissors"];
+const choicesArray = ["rock", "paper", "scissors"];
 
-function getComputerChoice() {
-    const compChoice = gameArray[Math.floor(Math.random() * gameArray.length)];
-    console.log(`The computer chose ${compChoice}. You lose!`);
+function getComputerChoice(choice) {
+    const choicePick = Math.floor(Math.random() * choice.length);
+    const compChoice = choice[choicePick];
+    console.log(`Computer chose ${compChoice}.`);
+    return;
 }
 
-function playRound(playerSelection, computerSelection) {
-    console.log
+function playerSelection() {
+    const playerOptions = prompt("Choose: rock, paper, scissors");
+    const playerChoice = playerOptions.toLowerCase();
+    return playerChoice;
 }
 
-playRound("rock", getComputerChoice);
+function computerSelection() {
+    
+}
+
+function playRound() {
+    console.log(`You chose ${playerSelection()}.`);
+    getComputerChoice(choicesArray);
+}
+
+playRound();
